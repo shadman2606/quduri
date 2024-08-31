71d51716d8d4c26a365a2eab7ec65885c5c93a54
Shadman Siddiqui <shadman2606@gmail.com>
2024-08-31 14:33:21 -0400

    Create update_progress.js

---
+++ update_progress.js
@@ -0,0 +1,14 @@
+classes = 20;
+pages = 35;
+
+class_progress = Math.round(classes*100/38);
+$('#class_progress').progress({
+  percent: class_progress
+});
+$('#classes_completed').text(classes);
+
+page_progress = Math.round(pages*100/192);
+$('#page_progress').progress({
+  percent: page_progress
+});
+$('#pages_completed').text(classes);
